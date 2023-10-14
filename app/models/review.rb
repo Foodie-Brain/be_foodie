a9d8373bf57f5d1d83e45d873a3ee36429a323a7
Zanna Fitch <zannafitch2004@gmail.com>
2023-10-14 16:38:13 -0500

    feat: Create validations for the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,18 @@
 class Review < ApplicationRecord
+
+  validates_presence_of :name, 
+                        :photo, 
+                        :description, 
+                        :lat, 
+                        :lon
+
+  validates_numericality_of :dairy_free, 
+                            :gluten_free, 
+                            :halal, 
+                            :kosher, 
+                            :nut_free, 
+                            :vegan, 
+                            :vegitarian, 
+                            :likes, 
+                            :dislikes
 end
