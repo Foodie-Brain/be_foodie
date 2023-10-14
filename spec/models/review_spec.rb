43c148c11067a294fca81977479523b73db3d066
Zanna Fitch <zannafitch2004@gmail.com>
2023-10-14 16:56:47 -0500

    test: test review validation

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:photo) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:lat) }
+    it { should validate_presence_of(:lon) }
+    it { should validate_numericality_of(:dairy_free) }
+    it { should validate_numericality_of(:gluten_free) }
+    it { should validate_numericality_of(:halal) }
+    it { should validate_numericality_of(:kosher) }
+    it { should validate_numericality_of(:nut_free) }
+    it { should validate_numericality_of(:vegan) }
+    it { should validate_numericality_of(:vegetarian) }
+    it { should validate_numericality_of(:likes) }
+    it { should validate_numericality_of(:dislikes) }
+  end
 end
