d6e3dd84d140ad4b76fc106de9da1bd1478997b4
Weston Schutt <wtschutt@gmail.com>
2023-10-14 11:13:10 -0600

    Chore: Update Review Factory with Faker data

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,18 +1,18 @@
 FactoryBot.define do
   factory :review do
-    name { "MyString" }
-    photo { "MyString" }
-    description { "MyString" }
-    dairy_free { 1 }
-    gluten_free { 1 }
-    halal { 1 }
-    kosher { 1 }
-    nut_free { 1 }
-    vegan { 1 }
-    vegitarian { 1 }
-    likes { 1 }
-    dislikes { 1 }
-    lat { "MyString" }
-    lon { "MyString" }
+    name { Faker::Food.dish }
+    photo { "#{Faker::JapaneseMedia::StudioGhibli.character}.jpg" }
+    description { Faker::Food.description }
+    dairy_free { Faker::Number.between(from: 0, to: 1) }
+    gluten_free { Faker::Number.between(from: 0, to: 1) }
+    halal { Faker::Number.between(from: 0, to: 1) }
+    kosher { Faker::Number.between(from: 0, to: 1) }
+    nut_free { Faker::Number.between(from: 0, to: 1) }
+    vegan { Faker::Number.between(from: 0, to: 1) }
+    vegitarian { Faker::Number.between(from: 0, to: 1) }
+    likes { Faker::Number.between(from: 0, to: 1000) }
+    dislikes { Faker::Number.between(from: 0, to: 1000) }
+    lat { Faker::Address.latitude }
+    lon { Faker::Address.longitude }
   end
 end
