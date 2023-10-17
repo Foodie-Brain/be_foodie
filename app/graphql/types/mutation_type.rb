54bff695403169f91b1d529047816cbe426df51d
Zanna Fitch <zannafitch2004@gmail.com>
2023-10-16 22:47:06 -0500

    Feat: create a delete query

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_review, mutation: Mutations::CreateReview
+    field :destroy_review, mutation: Mutations::DestroyReview
   end
 end
